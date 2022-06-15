e43e4d35119eebc39c52e23a7bbe1d14d43f06d4
dinhtruonggiang <dinhtruonggiang@gmail.com>
2022-06-15 21:01:43 +0700

    Delete Workshop2-Ownership-Borrowing/Exercise-1/target directory
    Delete target folder

--- Workshop2-Ownership-Borrowing/Exercise-1/target/debug/Exercise-1.d
+++
@@ -1,1 +0,0 @@
-F:\GIANG\VBI\VBI-Rust-Workshop\Workshop2-Ownership-Borrowing\Exercise-1\target\debug\Exercise-1.exe: F:\GIANG\VBI\VBI-Rust-Workshop\Workshop2-Ownership-Borrowing\Exercise-1\src\main.rs
--- Workshop2-Ownership-Borrowing/Exercise-1/target/debug/deps/Exercise_1.d
+++
@@ -1,5 +0,0 @@
-F:\GIANG\VBI\VBI-Rust-Workshop\Workshop2-Ownership-Borrowing\Exercise-1\target\debug\deps\Exercise_1.exe: src\main.rs
-
-F:\GIANG\VBI\VBI-Rust-Workshop\Workshop2-Ownership-Borrowing\Exercise-1\target\debug\deps\Exercise_1.d: src\main.rs
-
-src\main.rs:
