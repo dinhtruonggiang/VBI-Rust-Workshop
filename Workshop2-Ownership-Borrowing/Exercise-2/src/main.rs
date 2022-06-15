9c1fea4f89260b1b4964e8f770cb1ff19e6e4e36
Giang Dinh <giang@wheelpro.no>
2022-06-15 21:19:59 +0700

    Update logic

--- Workshop2-Ownership-Borrowing/Exercise-2/src/main.rs
+++ Workshop2-Ownership-Borrowing/Exercise-2/src/main.rs
@@ -3,18 +3,19 @@
 // Các bạn có thể sửa thêm logic để đúng với mục đichs bài này là liệt kê các số nguyên tố 
 fn main() {
     let mut count: u32 = 1;
+    let range = 10;
     let mut num: u64 = 1;
     let mut primes: Vec<u64> = Vec::new();
     primes.push(2);
 
-    while count < 10 {
+    while count < range {
         num += 2;
         if vector_is_prime(num, &primes) {            
             primes.push(num);
         }
         count += 1;
     }
-    println!("{:?}", primes);
+    println!("Các số nguyên tố trong phạm vi {} là: {:?}", range, primes);
 }
 
 fn vector_is_prime(num: u64, p: &Vec<u64>) -> bool {
