243cd7fddb2dfacd1fbcb69d556da02190f95106
Giang Dinh <giang@wheelpro.no>
2022-06-15 21:15:42 +0700

    Update logic

--- Workshop2-Ownership-Borrowing/Exercise-2/src/main.rs
+++ Workshop2-Ownership-Borrowing/Exercise-2/src/main.rs
@@ -19,7 +19,7 @@ fn main() {
 
 fn vector_is_prime(num: u64, p: &Vec<u64>) -> bool {
     for i in p {
-        if num > *i && num % i != 0 {
+        if num > *i && num % i == 0 {
             return false;
         }
     }
